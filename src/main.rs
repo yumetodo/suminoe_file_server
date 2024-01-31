ade3593700d7dd4fe3d7b47e861ee322278acdd8
yumetodo <yume-wikijp@live.jp>
2024-01-31 12:28:13 +0000

    feat: dir scanを実装

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use chrono::{serde::ts_seconds, DateTime, Utc};
+use serde::Serialize;
+use serde_json::Value;
+use std::fs;
+use std::path::Path;
+
+#[derive(Serialize)]
+struct DirectoryTreeResult {
+    dir: Option<Value>,
+    #[serde(with = "ts_seconds")]
+    last_scaned_at: DateTime<Utc>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let path = Path::new("/home/yumetodo/it_ims");
+    let tree = serde_json::to_string(&create_directory_tree(&path)).unwrap();
+    println!("{}", tree);
+}
+fn create_directory_tree(path: &Path) -> DirectoryTreeResult {
+    DirectoryTreeResult {
+        dir: tree_scan(path),
+        last_scaned_at: Utc::now(),
+    }
+}
+
+fn tree_scan(path: &Path) -> Option<Value> {
+    let name = path.file_name()?.to_str()?.to_string();
+    if name.starts_with('.') {
+        return None;
+    }
+    let mut tree: Vec<Value> = Vec::new();
+    tree.push(Value::String(name));
+    if path.is_dir() {
+        let entries = fs::read_dir(path).ok()?;
+        for entry in entries {
+            let entry = entry.ok()?;
+            if !entry.file_type().ok()?.is_dir() {
+                continue;
+            }
+            let child_path = entry.path();
+            let child_tree = tree_scan(&child_path);
+            if let Some(child_tree) = child_tree {
+                tree.push(child_tree);
+            }
+        }
+    }
+    Some(Value::Array(tree))
 }
